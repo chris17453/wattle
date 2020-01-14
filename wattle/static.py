07ee37b54f399654382a382c2ece730fd5dc83e0
Charles Watkins <chris17453@gmail.com>
2020-01-14 10:51:29 -0500

    up

--- wattle/static.py
+++ wattle/static.py
@@ -13,11 +13,51 @@ def unauth():
 @static.route('/home')
 @login_required
 def home():
-    return render_template("home.html")
+    return render_template("home.html",title="Home")
 
 @static.route('/login')
 def login():
-    return render_template("login.html",url="login")
+    return render_template("login.html",url="login",title="Login")
+
+@static.route('/bam/ipblocks/import')
+def bam_IP4BLOCKS_import():
+    blocks=[]
+    return render_template("bam_IP4BLOCKS_import.html",blocks=blocks,title="IP4Blocks Import")
+
+
+@static.route('/bam/ipblocks/list')
+def bam_IP4BLOCKS():
+    import importlib.util
+    spec = importlib.util.spec_from_file_location("bam_api", "/home/cwatkin1/repos/chris17453/ipam-o-nator/src/bam_api.py")
+    bam_api = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(bam_api)
+    
+    cred_file='/home/cwatkin1/repos/chris17453/ipam-o-nator/creds.json'
+
+    bam=bam_api.bam_api(creds=cred_file)
+    bam.login()    
+    data=bam.get_et()
+
+    blocks=[]
+    for item in data:
+        block={}
+        block['id'] =item['id']
+        block['name']=item['name']
+        block['type']=item['type']
+        tokens=item['properties'].split("|")
+        #block['properties']=''
+        for token in tokens:
+            try:
+                key,value =token.split('=')
+                block[key]=value
+            except:
+                pass
+                #if not token.strip().isspace():
+                #    block['properties']+="|"+token.strip()
+        blocks.append(block)
+
+    return render_template("bam_IP4BLOCKS.html",blocks=blocks,title="IP4Blocks List")
+
 
 @static.route('/js/<path:path>')
 def send_js(path):
@@ -32,7 +72,7 @@ def send_view(path):
     #return path
     f=open(path, "r")
     contents =f.read()
-    f.close();
+    f.close()
     json_string=json.dumps({'view':contents})
     return json_string
 
